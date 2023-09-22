095f0302d872504d49033cf0d8aad3a76e51e4bf
Romul1966 <romanalbul.067@gmail.com>
2023-09-22 13:11:52 +0300

    init

--- main.py
+++ main.py
@@ -5,8 +5,6 @@ import json
 headers = {
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 YaBrowser/23.7.5.734 Yowser/2.5 Safari/537.36"}
 
-content = []
-
 
 def get_url():
     url = "https://цдк-созвездие.рф/category/afisha/nab-v-kollekt/"
