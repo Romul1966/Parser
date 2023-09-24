d4009c96577014496442ea005a57468e1630d0c7
Romul1966 <romanalbul.067@gmail.com>
2023-09-24 13:48:02 +0300

    init

--- main.py
+++ main.py
@@ -6,6 +6,15 @@ headers = {
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 YaBrowser/23.7.5.734 Yowser/2.5 Safari/537.36"}
 
 
+#def check_post(id):
+  #  with open('list_card.json', encoding='utf-8') as file:
+     #   data = json.load(file)
+      #  for k in data.keys():
+        #    if id == k:
+            #    return True
+      #  return False
+
+
 def get_url():
     url = "https://цдк-созвездие.рф/category/afisha/nab-v-kollekt/"
     response = requests.get(url, headers=headers)
@@ -14,19 +23,30 @@ def get_url():
 
     for card in data:
         posts = card.find('div', class_='ports').get('id')
+
         list_url_name = 'https://цдк-созвездие.рф/' + card.find('a', rel="bookmark").get('href').replace(
             'https://xn----dtbecebkckn9b9a2d.xn--p1ai/', '')
+
         url_img = 'https://цдк-созвездие.рф/' + card.find('img').get('src').replace(
             'https://xn----dtbecebkckn9b9a2d.xn--p1ai/', '')
-        yield list_url_name, url_img, posts
+
+        yield posts, list_url_name, url_img
 
 
-file = open('list_card.json', 'w', encoding='utf-8')
-for url_name, img, post in get_url():
-    new = {"id": post, "img": img, "url_name": url_name}
+card_dict = {}
+file = open('list_card.json', 'a', encoding='utf-8')
+for post, url_name, img in get_url():
+
+   # if check_post(post):
+       # continue
+
+    card_dict[post] = {"img": img, "url_name": url_name}
+
     response = requests.get(url_name, headers=headers)
     soup = BeautifulSoup(response.text, 'lxml')
-    new["content"] = soup.find('div', class_='entry-content').text
-    json.dump(new, file, indent=2, ensure_ascii=False)
-    # new.clear()
+
+    content_text = soup.find('div', class_='entry-content').text
+    card_dict[post]["content"] = ' '.join([i.strip() for i in content_text.split()])
+
+json.dump(card_dict, file, indent=2, ensure_ascii=False)
 file.close()
